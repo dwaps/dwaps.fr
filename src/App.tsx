8bcb37485d233ce32464ec18647d9352ce68c68f
dwaps <ei.dwaps@gmail.com>
2024-01-20 11:08:57 +0100

    stop publish header and form login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import FormModal from "./components/FormModal";
-import HeaderBar from "./components/HeaderBar";
-import { ModalProvider } from "./utils/context/ModalContext";
 import "./firebase.config";
 import Home from "./pages/Home";
 import { AuthProvider } from "./utils/context/AuthContext";
 import { FormProvider } from "./utils/context/FormContext";
+import { ModalProvider } from "./utils/context/ModalContext";
 
 function App() {
   return (
@@ -13,8 +11,8 @@ function App() {
       <AuthProvider>
         <ModalProvider>
           <FormProvider>
-            <HeaderBar />
-            <FormModal />
+            {/* <HeaderBar /> */}
+            {/* <FormModal /> */}
             <Routes>
               <Route path="/" element={<Home />} />
             </Routes>
